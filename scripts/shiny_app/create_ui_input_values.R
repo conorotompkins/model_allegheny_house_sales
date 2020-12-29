5cc430aac5cb01efa628dc598534358980dbbd77
Conor Tompkins <conorotompkins@gmail.com>
2020-12-29 09:01:37 -0500

    wrote scripts to create inputs, read in inputs, and make shiny app

---
+++ scripts/shiny_app/create_ui_input_values.R
@@ -0,0 +1,105 @@
+library(tidyverse)
+library(tigris)
+library(sf)
+
+assessments <- read_csv("data/clean_assessment_data.csv")
+
+assessments %>% 
+  count(school_desc, sort = T) %>% 
+  write_csv("data/ui_input_values/school_desc_distinct.csv")
+
+assessments %>% 
+  count(style_desc, sort = T) %>%
+  write_csv("data/ui_input_values/style_desc_distinct.csv")
+
+assessments %>% 
+  distinct(grade_desc) %>% 
+  mutate(grade_desc = factor(grade_desc, levels = c("Highest Cost",
+                                                    "Excellent",
+                                                    "Very Good",
+                                                    "Good",
+                                                    "Average",
+                                                    "Below Average",
+                                                    "Poor"))) %>% 
+  arrange(grade_desc) %>% 
+  write_csv("data/ui_input_values/grade_desc_distinct.csv")
+
+assessments %>% 
+  count(condition_desc)
+
+assessments %>% 
+  distinct(condition_desc) %>% 
+  mutate(condition_desc = factor(condition_desc, levels = c(
+                                                    "Excellent",
+                                                    "Very Good",
+                                                    "Good",
+                                                    "Fair",
+                                                    "Average",
+                                                    "Poor",
+                                                    "Very Poor",
+                                                    "Unsound"))) %>% 
+  arrange(condition_desc) %>% 
+  write_csv("data/ui_input_values/condition_desc_distinct.csv")
+
+assessments %>%
+  filter(lot_area == min(lot_area)) %>%
+  distinct(lot_area) %>%
+  write_csv("data/ui_input_values/lot_area_range_min.csv")
+
+assessments %>%
+  filter(lot_area == quantile(lot_area, .7)) %>%
+  distinct(lot_area) %>%
+  write_csv("data/ui_input_values/lot_area_range_max.csv")
+
+assessments %>%
+  filter(finished_living_area == min(finished_living_area)) %>%
+  distinct(finished_living_area) %>%
+  write_csv("data/ui_input_values/finished_living_area_min.csv")
+
+assessments %>%
+  filter(finished_living_area == quantile(finished_living_area, .95)) %>%
+  distinct(finished_living_area) %>%
+  write_csv("data/ui_input_values/finished_living_area_max.csv")
+
+assessments %>% 
+  filter(total_rooms == min(total_rooms)) %>% 
+  distinct(total_rooms) %>% 
+  write_csv("data/ui_input_values/total_rooms_min.csv")
+
+assessments %>% 
+  filter(total_rooms == max(total_rooms)) %>% 
+  distinct(total_rooms) %>% 
+  write_csv("data/ui_input_values/total_rooms_max.csv")
+
+assessments %>% 
+  filter(year_blt == min(year_blt)) %>% 
+  distinct(year_blt) %>% 
+  write_csv("data/ui_input_values/year_blt_min.csv")
+
+assessments %>% 
+  filter(year_blt == max(year_blt)) %>% 
+  distinct(year_blt) %>% 
+  write_csv("data/ui_input_values/year_blt_max.csv")
+
+
+#county shapefile
+counties(state = "PA", cb = TRUE) %>% 
+  filter(NAME == "Allegheny") %>% 
+  st_write("data/ui_input_values/allegheny_county.shp")
+
+tigris::area_water("PA", "Allegheny") %>% 
+  group_by(FULLNAME) %>% 
+  summarize(AWATER = sum(AWATER)) %>% 
+  ungroup() %>% 
+  arrange(desc(AWATER)) %>% 
+  slice(1:3) %>% 
+  st_write("data/ui_input_values/allegheny_water.shp")
+
+# st_read("data/ui_input_values/allegheny_county.shp") %>% 
+#   ggplot() +
+#   geom_sf()
+# 
+# st_read("data/ui_input_values/allegheny_water.shp") %>% 
+#   ggplot() +
+#   geom_sf()
+
---
+++ scripts/shiny_app/read_ui_input_values.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(sf)
+
+#load data
+lot_area_summary <- read_csv("data/lot_area_summary.csv")
+finished_living_area_summary <- read_csv("data/finished_living_area_summary.csv")
+
+#assessments <- read_csv("data/clean_assessment_data.csv")
+
+full_results <- read_csv("output/full_model_results.csv")
+
+model_fit <- read_rds("data/model_fit.rds")
+
+school_desc_distinct <- read_csv("data/ui_input_values/school_desc_distinct.csv")
+
+style_desc_distinct <- read_csv("data/ui_input_values/style_desc_distinct.csv")
+
+grade_desc_distinct <- read_csv("data/ui_input_values/grade_desc_distinct.csv")
+
+condition_desc_distinct <- read_csv("data/ui_input_values/condition_desc_distinct.csv")
+
+lot_area_range_min <- read_csv("data/ui_input_values/lot_area_range_min.csv")
+
+lot_area_range_max <- read_csv("data/ui_input_values/lot_area_range_max.csv")
+
+finished_living_area_min <- read_csv("data/ui_input_values/finished_living_area_min.csv")
+
+finished_living_area_max <- read_csv("data/ui_input_values/finished_living_area_max.csv")
+
+total_rooms_min <- read_csv("data/ui_input_values/total_rooms_min.csv")
+
+total_rooms_max <- read_csv("data/ui_input_values/total_rooms_max.csv")
+
+year_blt_min <- read_csv("data/ui_input_values/year_blt_min.csv")
+
+year_blt_max <- read_csv("data/ui_input_values/year_blt_max.csv")
+
+#mapping
+ac_boundary <- st_read("data/ui_input_values/allegheny_county.shp")
+ac_water <- st_read("data/ui_input_values/allegheny_water.shp")
