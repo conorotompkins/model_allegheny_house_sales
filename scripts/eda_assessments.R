1ed1029bf84e6a67e3cc6d355bde95333a2d5189
Conor Tompkins <conorotompkins@gmail.com>
2021-01-27 09:42:35 -0500

    added more EDA steps

--- scripts/eda_assessments.R
+++ scripts/eda_assessments.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(hrbrthemes)
 library(janitor)
+library(scales)
 
 options(scipen = 999, digits = 4)
 
@@ -21,59 +22,98 @@ assessments_valid %>%
 assessments_valid %>% 
   ggplot(aes(sale_year, sale_price)) +
   geom_density_2d_filled() +
-  scale_y_log10()
+  scale_y_log10(label = dollar)
 
 assessments_valid %>% 
   ggplot(aes(sale_year, sale_price_adj)) +
   geom_density_2d_filled() +
-  scale_y_log10()
+  scale_y_log10(label = dollar)
 
 assessments_valid %>%
   select(sale_year, sale_price, sale_price_adj) %>% 
   pivot_longer(cols = contains("sale_price")) %>% 
   ggplot(aes(sale_year, value)) +
-  geom_density_2d_filled(contour_var = "ndensity") +
+  geom_density_2d_filled() +
   facet_wrap(~name, ncol = 1) +
-  scale_y_log10(labels = scales::dollar_format())
+  scale_y_log10(labels = dollar)
+
+assessments_valid %>%
+  select(sale_year, sale_price, sale_price_adj) %>% 
+  pivot_longer(cols = contains("sale_price")) %>% 
+  ggplot(aes(sale_year, value, color = name)) +
+  geom_smooth() +
+  scale_y_log10(labels = dollar)
+
+assessments_valid %>%
+  select(sale_year, sale_price, sale_price_adj) %>% 
+  ggplot(aes(sale_price_adj, sale_price)) +
+  geom_point(alpha = .01)  +
+  coord_cartesian(xlim = c(10^1, 10^8),
+                  ylim = c(10^1, 10^8)) +
+  scale_x_log10(labels = dollar) +
+  scale_y_log10(labels = dollar)
 
 assessments_valid %>% 
   ggplot(aes(house_age_at_sale, sale_price_adj)) +
   geom_density_2d_filled() +
-  scale_y_log10()
+  scale_y_log10(labels = dollar)
 
 assessments_valid %>% 
   ggplot(aes(lot_area, sale_price_adj)) +
   geom_density_2d_filled() +
-  scale_y_log10() +
-  coord_cartesian(xlim = c(0, 10))
+  scale_y_log10(labels = dollar) +
+  coord_cartesian(xlim = c(0, 50000))
+
+assessments_valid %>% 
+  mutate(geo_id = fct_reorder(geo_id, lot_area, .fun = median)) %>% 
+  ggplot(aes(lot_area, geo_id)) +
+  geom_boxplot(outlier.alpha = .01) +
+  coord_cartesian(xlim = c(0, 10^5))
+
+assessments_valid %>% 
+  mutate(style_desc = fct_reorder(style_desc, lot_area, .fun = median)) %>% 
+  ggplot(aes(lot_area, style_desc)) +
+  geom_boxplot(outlier.alpha = .01) +
+  coord_cartesian(xlim = c(0, 10^5))
 
 assessments_valid %>% 
   ggplot(aes(finished_living_area, sale_price_adj)) +
   geom_density_2d_filled() +
-  scale_y_log10() +
+  scale_y_log10(labels = dollar) +
   coord_cartesian(xlim = c(0, 5000))
 
 assessments_valid %>% 
-  distinct(grade_desc)
+  mutate(style_desc = fct_reorder(style_desc, finished_living_area, .fun = median)) %>% 
+  ggplot(aes(finished_living_area, style_desc)) +
+  geom_boxplot(outlier.alpha = .01) +
+  coord_cartesian(xlim = c(0, 10^4))
 
 assessments_valid %>% 
-  mutate(condition_desc = fct_reorder(condition_desc, sale_price_adj, median)) %>% 
-  ggplot(aes(sale_price_adj, condition_desc)) +
-  geom_boxplot() +
-  scale_x_log10()
+  mutate(style_desc = fct_reorder(style_desc, lot_area, .fun = median)) %>% 
+  ggplot(aes(lot_area, style_desc)) +
+  geom_boxplot(outlier.alpha = .01) +
+  coord_cartesian(xlim = c(0, 10^5))
 
 assessments_valid %>% 
   mutate(grade_desc = fct_reorder(grade_desc, sale_price_adj, median)) %>% 
   ggplot(aes(sale_price_adj, grade_desc)) +
-  geom_boxplot() +
-  scale_x_log10()
-  
+  geom_boxplot(outlier.alpha = .1) +
+  scale_x_log10(labels = dollar)
+
+assessments_valid %>% 
+  mutate(condition_desc = fct_reorder(condition_desc, sale_price_adj, median)) %>% 
+  ggplot(aes(sale_price_adj, condition_desc)) +
+  geom_boxplot(outlier.alpha = .3) +
+  scale_x_log10(labels = dollar)
+
+
 assessments_valid %>% 
   arrange(desc(sale_price_adj)) %>% 
   slice(1:5)
 
 assessments_valid %>% 
   add_count(style_desc) %>% 
+  filter(n > 5000) %>% 
   mutate(style_desc = fct_reorder(style_desc, n, .fun = "max", .desc = T)) %>% 
   ggplot(aes(year_blt)) +
   geom_histogram() +
@@ -84,13 +124,16 @@ assessments_valid %>%
   ggplot(aes(finished_living_area, fill = geo_id)) +
   geom_density(alpha = .5)
 
+#identify cutoff
 assessments_valid %>% 
   ggplot(aes(lot_area)) +
   geom_density() +
   geom_vline(xintercept = 40000) +
   scale_x_log10()
 
+
 assessments_valid %>% 
   ggplot(aes(finished_living_area)) +
   geom_density() +
-  scale_x_log10()
+  scale_x_log10() +
+  geom_vline(xintercept = 10000)
