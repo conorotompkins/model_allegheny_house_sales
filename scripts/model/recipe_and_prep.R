dc6da55864fd2e59489ebdf8d918c5e011743287
Conor Tompkins <conorotompkins@gmail.com>
2021-02-04 09:59:26 -0500

    removed lot size and finished living area scaling code
    deleted code that scales lot size and finished living area, deleted commented out code

--- scripts/model/recipe_and_prep.R
+++ scripts/model/recipe_and_prep.R
@@ -9,8 +9,6 @@ library(future)
 library(lobstr)
 library(butcher)
 
-#plan(strategy = "multisession")
-
 options(scipen = 999)
 theme_set(theme_ipsum())
 
@@ -26,24 +24,12 @@ skim(assessments_valid)
 
 housing_sales <- assessments_valid %>% 
   left_join(parcel_geo, by = c("par_id" = "pin")) %>% 
-  #mutate(sale_price_adj_log10 = log10(sale_price_adj)) %>% 
   select(-sale_price) %>% 
   select(everything(), longitude, latitude) %>% 
   select(par_id, sale_price_adj, house_age_at_sale, lot_area, 
          finished_living_area, bedrooms, fullbaths, halfbaths, geo_id, 
          style_desc, grade_desc, condition_desc,
          longitude, latitude)
-
-#normalize lot_area and finished_living_area
-housing_sales <- housing_sales %>% 
-  group_by(geo_id) %>% 
-  mutate(lot_area_zscore = scale(lot_area) %>% as.vector()) %>% 
-  ungroup() %>% 
-  group_by(style_desc) %>% 
-  mutate(finished_living_area_zscore = scale(finished_living_area) %>% as.vector()) %>% 
-  ungroup() %>% 
-  select(-c(finished_living_area, lot_area))
-##
 
 glimpse(housing_sales)
 # Fix the random numbers by setting the seed 
@@ -55,27 +41,6 @@ data_split <- initial_split(housing_sales, prop = 3/4)
 # Create data frames for the two sets:
 train_data <- training(data_split)
 test_data  <- testing(data_split)
-
-# model <- lm(log10(sale_price_log10) ~ year_blt + calcacreag + totalrooms + finishedlivingarea + school_desc  + style_desc, data = housing_sales)
-# 
-# model %>% 
-#   tidy() %>% 
-#   filter(term != "(Intercept)") %>% 
-#   mutate(term = fct_reorder(term, estimate)) %>% 
-#   ggplot(aes(estimate, term)) +
-#   geom_point()
-
-assessments_valid %>% 
-  mutate(condition_desc = fct_relevel(condition_desc, "Average")) %>% 
-  arrange(condition_desc) %>% 
-  count(condition_desc)
-
-assessments_valid %>% 
-  select(condition_desc) %>% 
-  mutate(condition_desc_new = case_when(condition_desc %in% c("Poor", "Very Poor", "Unsound") ~ "Poor or worse",
-                                        condition_desc %in% c("Very Good", "Excellent") ~ "Very Good or better",
-                                        TRUE ~ condition_desc)) %>% 
-  count(condition_desc, condition_desc_new)
 
 #create recipe
 model_recipe <- recipe(sale_price_adj ~ .,
