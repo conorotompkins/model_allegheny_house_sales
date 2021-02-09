e811e2d1b2c819a478247bc8843b7ec7dbc3d25e
Conor Tompkins <conorotompkins@gmail.com>
2021-02-09 17:08:17 -0500

    pushed changes for redesigned app UI and plotly graph + other changes. from house_price_estimator_test

--- house_price_estimator/server.R
+++ house_price_estimator/server.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(shinythemes)
 library(shinyWidgets)
+library(plotly)
+
 library(tidyverse)
 library(tidymodels)
 library(hrbrthemes)
@@ -16,10 +18,7 @@ geo_id_style_desc <- read_csv("geo_id_style_desc.csv")
 
 geo_id_shapes <- st_read("unified_geo_ids/unified_geo_ids.shp")
 
-#lot_area_summary <- read_csv("house_price_estimator_test/lot_area_summary.csv")
-#finished_living_area_summary <- read_csv("house_price_estimator_test/finished_living_area_summary.csv")
-
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   #create data to predict on
   predict_data_reactive <- reactive({
@@ -42,22 +41,9 @@ server <- function(input, output) {
            heat_type = input$heat_type_choice,
            ac_flag = as.logical(input$ac_flag_choice),
            longitude = 1,
-           latitude = 1) #%>% 
-      # left_join(finished_living_area_summary) %>% 
-      # left_join(lot_area_summary) %>% 
-      # mutate(finished_living_area_zscore = (finished_living_area - finished_living_area_mean) / finished_living_area_sd,
-      #        lot_area_zscore = (lot_area - lot_area_mean) / lot_area_sd) %>% 
-      # select(-c(matches("mean$|sd$"), lot_area, finished_living_area))
+           latitude = 1)
     
   })
-  
-  # baked_data <- reactive({
-  #   
-  #   #read_rds("house_sale_estimator/model_recipe_prepped.rds") %>% 
-  #   prepped_model_recipe %>% 
-  #     bake(predict_data_reactive())
-  #   
-  # })
   
   predictions_reactive <- reactive({
     
@@ -76,14 +62,33 @@ server <- function(input, output) {
       semi_join(predict_data_reactive(), by = c("geo_id", "style_desc"))
   })
   
-  plot_parameters_reactive <- reactive({
-    representative_sample_reactive() %>%
-      pull(sale_price_adj) %>%
-      hist(breaks = 30) %>% 
-      .$counts %>% 
-      enframe() %>%
-      summarize(max_count = max(value)) %>%
-      pull(max_count)
+  style_desc_bar_graph_data_reactive <- reactive({
+    
+    req(selected_geo_id())
+    geo_id_style_desc %>% 
+      filter(geo_id == selected_geo_id())
+    
+  })
+  
+  output$style_desc_bar_graph <- renderPlotly({
+    
+    req(style_desc_bar_graph_data_reactive())
+    
+    plotly_graph <- style_desc_bar_graph_data_reactive() %>% 
+      count(style_desc, sort = T) %>% 
+      mutate(style_desc = fct_lump_n(style_desc, n = 5, w = n, other_level = "Other")) %>% 
+      mutate(style_desc = fct_reorder(style_desc, n)) %>% 
+      ggplot(aes(n, style_desc)) +
+      geom_col() +
+      scale_x_comma() +
+      labs(title = str_c("Top house styles in", selected_geo_id(), sep = " "),
+           x = "Count of sales",
+           y = NULL) +
+      theme_ipsum(base_size = 20) +
+      theme(axis.title.x = element_text(size = 18, hjust = .5),
+            axis.title.y = element_text(size = 20))
+    
+    ggplotly(plotly_graph, height = 400)
     
   })
   
@@ -92,16 +97,17 @@ server <- function(input, output) {
          str_c("Grade:", input$grade_desc_choice, sep = " "), 
          str_c("Condition:", input$condition_desc_choice, sep = " "),
          str_c("Style:", input$style_desc_choice, sep = " "),
-         str_c("Lot area:", comma(input$lot_area_choice), sep = " "),
-         str_c("Finished living area:", comma(input$finished_living_area_choice), sep = " "),
+         str_c("Lot area:", comma(input$lot_area_choice), "sq. ft.", sep = " "),
+         str_c("Finished living area:", comma(input$finished_living_area_choice), "sq. ft.", sep = " "),
          str_c("Bedrooms:", input$bedrooms_choice, sep = " "),
          str_c("Full Bathrooms:", input$fullbaths_choice, sep = " "),
          str_c("Half Bathrooms:", input$halfbaths_choice, sep = " "),
          str_c("Year Built:", input$year_blt_choice, sep = " "),
          str_c("Heat Source:", input$heat_type_choice, sep = " "),
          str_c("Air Conditioning:", input$ac_flag_choice, sep = " "),
-         str_c("Sale Month:", input$sale_month_choice, sep = " ")
-         ) %>% 
+         str_c("Sale Month:", input$sale_month_choice, sep = " "),
+         str_c("Predicted price:", dollar(predictions_reactive()$.pred), sep = " ")
+    ) %>% 
       glue::glue_collapse(sep = "\n")
   })
   
@@ -122,9 +128,11 @@ server <- function(input, output) {
   
   output$model_output_graph <- renderPlot({
     
+    binwidth_calc <- IQR(representative_sample_reactive()$sale_price_adj) / 10
+    
     representative_sample_reactive() %>%
       ggplot(aes(x = sale_price_adj)) +
-      geom_histogram(fill = "grey", color = "black", binwidth = 50000) +
+      geom_histogram(fill = "grey", color = "black", binwidth = binwidth_calc) +
       # annotate(geom = "rect",
       #          xmin = predictions_reactive()$.pred_lower, xmax = predictions_reactive()$.pred_upper,
       #          ymin = 0, ymax = Inf, fill = "#FCCF02", alpha = .7) +
@@ -133,32 +141,18 @@ server <- function(input, output) {
                  size = 2) +
       scale_x_continuous(labels = scales::dollar_format()) +
       scale_y_comma() +
-      #coord_cartesian(ylim = c(0, plot_parameters_reactive() * 1.4)) +
       labs(title = str_c(nrow(representative_sample_reactive()) %>% comma(), "sales of",
                          distinct(representative_sample_reactive())$style_desc, "homes in",
                          distinct(representative_sample_reactive())$geo_id,
                          sep = " "),
-           x = "Sale Price",
+           x = "Actual Sale Price",
            y = "Count of similar homes") +
-      theme_ipsum(base_size = 20) +
+      theme_ipsum(base_size = 24) +
       theme(panel.background = element_rect(fill = "black"),
-            axis.title.x = element_text(size = 18),
-            axis.title.y = element_text(size = 18))
+            axis.title.x = element_text(size = 20, hjust = .5),
+            axis.title.y = element_text(size = 20))
     
   })
-  
-  # output$school_desc_map <- renderPlot({
-  #   
-  #   #full_results %>% 
-  #   school_district_shapes %>% 
-  #     semi_join(predict_data_reactive(), by = "school_desc") %>% 
-  #     ggplot() +
-  #     geom_sf(data = ac_boundary, fill = "black") +
-  #     geom_sf(data = ac_water, fill = "white") +
-  #     geom_sf(fill = "#FCCF02", color = "#FCCF02", alpha = .7, size = NA) +
-  #     theme_void()
-  #   
-  # })
   
   output$leaflet_title <- renderText("Click on a region to start")
   
@@ -171,38 +165,49 @@ server <- function(input, output) {
                                                      minZoom = 9, 
                                                      #maxZoom = 8
                        )) %>% 
-      setView(lng = -80.01181092430839, lat = 40.44170119122286, zoom = 9) %>% 
+      setView(lng = -80.01181092430839, lat = 40.44170119122286, zoom = 10) %>% 
       setMaxBounds(lng1 = -79.5, lng2 = -80.5, lat1 = 40.1, lat2 = 40.7) %>% 
       addPolygons(layerId = ~geo_id,
-                  fillColor = "#FCCF02",
+                  fillColor = "#000000",
                   fillOpacity = .7,
                   stroke = TRUE,
-                  color = "black",
+                  color = "#FCCF02",
                   weight = 1)
   })
   
   #capture click from leaflet map
   selected_geo_id <- reactive({input$geo_id_map_shape_click$id})
   
+  #update style_desc_choice input list
+  observeEvent(selected_geo_id(), {
+    
+    req(selected_geo_id())
+    updated_choices <- style_desc_bar_graph_data_reactive() %>%
+      count(style_desc, sort = T) %>%
+      pull(style_desc)
+    
+    updateSelectInput(session,
+                      inputId = "style_desc_choice",
+                      choices = updated_choices)
+  })
+  
+  
   observe({ #observer
     
     req(selected_geo_id())
-    
-    # if (length(selected_school_desc()) == 0)
-    #   return()
-    # 
-    # else {
     
     #filter and map
     leafletProxy("geo_id_map", data = filter(geo_id_shapes, geo_id == input$geo_id_map_shape_click$id)) %>%
       clearGroup("highlight_shape") %>% 
       clearGroup("popup") %>% 
-      addPolygons(group = "highlight_shape") %>% 
+      addPolygons(group = "highlight_shape",
+                  fillColor = "#FCCF02",
+                  fillOpacity = 1,
+                  color = "#FCCF02") %>% 
       addPopups(popup = ~geo_id,
                 group = "popup",
                 lng = ~lng,
                 lat = ~lat)
-    #}
   }) #observer
   
   output$credits_1 <- renderText("Dashboard created by Conor Tompkins with R + Leaflet")
